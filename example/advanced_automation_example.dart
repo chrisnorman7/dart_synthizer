2887b1e880da162507ed373d95eb05f612947c10
Chris Norman <chris.norman2@googlemail.com>
2021-09-26 15:46:49 +0100

    Finished automation and added an example.

---
+++ example/advanced_automation_example.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'dart:math';
+
+import 'package:dart_synthizer/dart_synthizer.dart';
+
+const stepsBeforeWait = 10;
+const secondsPerStep = 0.1;
+const degreesPerStep = 5.0;
+void enqueueEvents(Source3D source, Context ctx, int itersSoFar) {
+  final commands = <AutomationCommand>[
+    AutomationSendUserEventCommand(
+        (itersSoFar + stepsBeforeWait) * secondsPerStep,
+        itersSoFar + stepsBeforeWait)
+  ];
+  for (var i = 0; i < stepsBeforeWait; i++) {
+    final iter = itersSoFar + i;
+    commands.insert(
+        0,
+        AutomationAppendPropertyCommand(
+            iter * secondsPerStep,
+            Properties.position,
+            Double3(10.0 * sin(iter * degreesPerStep * pi / 180.0),
+                10.0 * cos(iter * degreesPerStep * pi / 180.0), 0.0)));
+  }
+  ctx.executeAutomation(source, commands);
+}
+
+Future<void> main() async {
+  var itersSoFar = 0;
+  final synthizer = Synthizer()
+    ..initialize(
+        logLevel: LogLevel.debug, loggingBackend: LoggingBackend.stderr);
+  final ctx = synthizer.createContext(events: true)
+    ..orientation = Double6(0.0, 1.0, 0.0, 0.0, 0.0, 1.0);
+  final source = ctx.createSource3D();
+  final buffer = Buffer.fromFile(synthizer, File('sound.wav'));
+  final generator = ctx.createBufferGenerator(buffer: buffer)..looping = true;
+  source.addGenerator(generator);
+  final listener = ctx.events.listen((event) {
+    if (event is UserAutomationEvent) {
+      print('Starting iteration $itersSoFar.');
+      itersSoFar = event.param;
+      enqueueEvents(source, ctx, itersSoFar);
+    }
+  });
+  enqueueEvents(source, ctx, itersSoFar);
+  stdin
+    ..echoMode = false
+    ..lineMode = false
+    ..listen((event) {
+      listener.cancel();
+      for (final thing in [ctx, source, generator, buffer]) {
+        thing.destroy();
+      }
+      synthizer.shutdown();
+      exit(0);
+    });
+}
--- lib/automation.dart
+++ lib/automation.dart
@@ -6,6 +6,8 @@ import 'package:ffi/ffi.dart';
 import 'classes.dart';
 import 'context.dart';
 import 'enumerations.dart';
+import 'error.dart';
+import 'properties.dart';
 import 'synthizer.dart';
 import 'synthizer_bindings.dart';
 
@@ -32,7 +34,16 @@ class AutomationAppendPropertyCommand extends AutomationCommand {
   final Properties property;
 
   /// The value of this point.
-  final double value;
+  ///
+  /// Unfortunately, until such time as we have unions in Dart, we have to make
+  /// this value dynamic.
+  ///
+  /// Values have the following meanings:
+  ///
+  /// * Double values work as you would expect.
+  /// * [Double3] and [Double6] values will be broken up by the
+  /// [AutomationBatch.addCommands] method.
+  final dynamic value;
 
   /// The interpolation type to use.
   final InterpolationTypes interpolationType;
@@ -96,9 +107,25 @@ class AutomationBatch extends SynthizerObject {
       if (command is AutomationAppendPropertyCommand) {
         final append = ref.params.append_to_property
           ..property = command.property.toInt();
-        append.point
-          ..interpolation_type = command.interpolationType.toInt()
-          ..values[0] = command.value;
+        append.point.interpolation_type = command.interpolationType.toInt();
+        final dynamic value = command.value;
+        if (value is double) {
+          append.point.values[0] = value;
+        } else if (value is Double3) {
+          append.point.values[0] = value.x;
+          append.point.values[1] = value.y;
+          append.point.values[2] = value.z;
+        } else if (value is Double6) {
+          append.point.values[0] = value.x1;
+          append.point.values[1] = value.y1;
+          append.point.values[2] = value.z1;
+          append.point.values[3] = value.x2;
+          append.point.values[4] = value.y2;
+          append.point.values[5] = value.z2;
+        } else {
+          throw SynthizerError(
+              'Invalid point type ${value.runtimeType}: $value.', -1);
+        }
       } else if (command is AutomationClearPropertyCommand) {
         ref.params.clear_property.property = command.property.toInt();
       } else if (command is AutomationSendUserEventCommand) {
