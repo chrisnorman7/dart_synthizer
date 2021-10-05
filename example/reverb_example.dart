30eec891810ddf320e720abbca98958f5ecddffa
Chris Norman <chris.norman2@googlemail.com>
2021-10-05 20:48:00 +0100

    Added a reverb example.

--- example/reverb_example.dart
+++ example/reverb_example.dart
@@ -9,11 +9,15 @@ Future<void> main() async {
   final generator = ctx.createStreamingGenerator('file', 'sound.wav')
     ..looping = true;
   source.addGenerator(generator);
-  final reverb = ctx.createGlobalFdnReverb()
+  final reverb = ctx.createGlobalFdnReverb();
+  await Future<void>.delayed(Duration(milliseconds: 200));
+  reverb
     ..t60 = 5
     ..meanFreePath = 0.5;
   ctx.ConfigRoute(source, reverb);
   await Future<void>.delayed(Duration(seconds: 5));
+  print(reverb.meanFreePath);
+  print(reverb.t60);
   [source, ctx, generator, reverb].forEach((element) => element.destroy());
   synthizer.shutdown();
 }
